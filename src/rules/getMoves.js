fe9cff099a69309184cd18ef3e11dfd9b460b897
ishanyadav13290 <ishanyadav13290@gmail.com>
2023-10-06 19:25:01 +0530

    knight/horse's moves added

--- src/rules/getMoves.js
+++ src/rules/getMoves.js
@@ -29,3 +29,30 @@ export const getRookMoves = ({position,piece,rank,file}) => {
 
   return moves
 }
+
+export const getKnightMoves = ({position,rank,file}) => {
+    const moves = []
+    const enemy = position[rank][file].startsWith('w')?'b':'w'
+
+    const candidates = [
+        [-2,-1],
+        [-2,1],
+        [2,-1],
+        [2,1],
+        [1,-2],
+        [1,2],
+        [-1,-2],
+        [-1,2],
+    ] //possible moves of knight's moves
+
+    candidates.forEach(c=>{
+        const cell = position?.[rank+c[0]]?.[file+c[1]]
+        if(cell !== undefined && (cell.startsWith(enemy) || cell==='')){
+            moves.push([rank+c[0],file+c[1]])
+        }
+    })
+    return moves
+  }
+
+  
+  

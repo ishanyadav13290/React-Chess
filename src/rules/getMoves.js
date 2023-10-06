fac3f495dc1ff06008f721dad93f653f4a0b2220
ishanyadav13290 <ishanyadav13290@gmail.com>
2023-10-06 19:56:42 +0530

    bishop, queen, and kings moves added

--- src/rules/getMoves.js
+++ src/rules/getMoves.js
@@ -1,58 +1,118 @@
-export const getRookMoves = ({position,piece,rank,file}) => {
-  const moves = []
-  const us = piece[0]
-  const enemy = us === 'w' ? 'b' : 'w'
+export const getRookMoves = ({ position, piece, rank, file }) => {
+  const moves = [];
+  const us = piece[0];
+  const enemy = us === "w" ? "b" : "w";
 
   const direction = [
-      [-1,0],
-      [1,0],
-      [0,-1],
-      [0,1],
-  ]
+    [-1, 0],
+    [1, 0],
+    [0, -1],
+    [0, 1],
+  ];
 
-  direction.forEach(dir => {
-      for (let i = 1; i < 8; i++) {
-          const x = rank + (i*dir[0])
-          const y = file + (i*dir[1])
-          if(position?.[x]?.[y] === undefined)
-              break
-          if(position[x][y].startsWith(enemy)){
-              moves.push ([x,y])
-              break;
-          }
-          if(position[x][y].startsWith(us)){
-              break
-          }
-          moves.push ([x,y])
+  direction.forEach((dir) => {
+    for (let i = 1; i < 8; i++) {
+      const x = rank + i * dir[0];
+      const y = file + i * dir[1];
+      if (position?.[x]?.[y] === undefined) break;
+      if (position[x][y].startsWith(enemy)) {
+        moves.push([x, y]);
+        break;
       }
-  })
+      if (position[x][y].startsWith(us)) {
+        break;
+      }
+      moves.push([x, y]);
+    }
+  });
 
-  return moves
-}
+  return moves;
+};
 
-export const getKnightMoves = ({position,rank,file}) => {
-    const moves = []
-    const enemy = position[rank][file].startsWith('w')?'b':'w'
+export const getKnightMoves = ({ position, rank, file }) => {
+  const moves = [];
+  const enemy = position[rank][file].startsWith("w") ? "b" : "w";
 
-    const candidates = [
-        [-2,-1],
-        [-2,1],
-        [2,-1],
-        [2,1],
-        [1,-2],
-        [1,2],
-        [-1,-2],
-        [-1,2],
-    ] //possible moves of knight's moves
+  const candidates = [
+    [-2, -1],
+    [-2, 1],
+    [2, -1],
+    [2, 1],
+    [1, -2],
+    [1, 2],
+    [-1, -2],
+    [-1, 2],
+  ]; //possible moves of knight's moves
 
-    candidates.forEach(c=>{
-        const cell = position?.[rank+c[0]]?.[file+c[1]]
-        if(cell !== undefined && (cell.startsWith(enemy) || cell==='')){
-            moves.push([rank+c[0],file+c[1]])
-        }
-    })
-    return moves
-  }
+  candidates.forEach((c) => {
+    const cell = position?.[rank + c[0]]?.[file + c[1]];
+    if (cell !== undefined && (cell.startsWith(enemy) || cell === "")) {
+      moves.push([rank + c[0], file + c[1]]);
+    }
+  });
+  return moves;
+};
 
-  
-  
+export const getBishopMoves = ({ position, piece, rank, file }) => {
+  const moves = [];
+  const us = piece[0];
+  const enemy = us === "w" ? "b" : "w";
+
+  const direction = [
+    [-1, -1],
+    [-1, 1],
+    [1, -1],
+    [1, 1],
+  ];
+
+  direction.forEach((dir) => {
+    for (let i = 1; i < 8; i++) {
+      const x = rank + i * dir[0];
+      const y = file + i * dir[1];
+      if (position?.[x]?.[y] === undefined) break;
+      if (position[x][y].startsWith(enemy)) {
+        moves.push([x, y]);
+        break;
+      }
+      if (position[x][y].startsWith(us)) {
+        break;
+      }
+      moves.push([x, y]);
+    }
+  });
+
+  return moves;
+};
+
+export const getQueenMoves = ({ position, piece, rank, file }) => {
+  const moves = [
+    ...getBishopMoves({ position, piece, rank, file }),
+    ...getRookMoves({ position, piece, rank, file }),
+  ];
+
+  return moves;
+};
+
+export const getKingMoves = ({ position, rank, file }) => {
+  const moves = [];
+  const enemy = position[rank][file].startsWith("w") ? "b" : "w";
+
+  const candidates = [
+    [-1, 0],
+    [-1, 1],
+    [1, 0],
+    [1, -1],
+    [1, 1],
+    [0, -1],
+    [0, 1],
+    [-1, -1],
+  ]; //possible moves of knight's moves
+
+  candidates.forEach((c) => {
+    const cell = position?.[rank + c[0]]?.[file + c[1]]; //current position[mainArray+possibleArrayLocationIndex]
+    if (cell !== undefined && (cell.startsWith(enemy) || cell === "")) {
+      moves.push([rank + c[0], file + c[1]]);
+    }
+  });
+  return moves;
+};
--- src/rules/rules.js
+++ src/rules/rules.js
@@ -1,8 +1,11 @@
-import { getKnightMoves, getRookMoves } from "./getMoves"
+import { getBishopMoves, getKingMoves, getKnightMoves, getQueenMoves, getRookMoves } from "./getMoves"
 
 const rules ={
 
     getRegularMoves: function({position,piece,rank,file}){
+        if(piece.endsWith('k'))return getKingMoves({position,piece,rank,file})
+        if(piece.endsWith('q'))return getQueenMoves({position,piece,rank,file})
+        if(piece.endsWith('b'))return getBishopMoves({position,piece,rank,file})
         if(piece.endsWith('n'))return getKnightMoves({position,rank,file})
         if(piece.endsWith('r')) return getRookMoves({position,piece,rank,file})
     }
